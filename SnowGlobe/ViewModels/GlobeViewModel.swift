df996832b07005a5881fdb3d5d4333604db4ee7e
Saverio Negro <saverionegro@gmail.com>
2025-04-27 16:08:15 +0200

    Create `GlobeViewModel` to collect user inputs, from `ContentView` as to which features of the globe to display, and share with and use that data within `GlobeView` to display entities accordingly.

---
+++ SnowGlobe/ViewModels/GlobeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  GlobeViewModel.swift
+//  SnowGlobe
+//
+//  Created by Saverio Negro on 27/04/25.
+//
+
+import SwiftUI
+
+@Observable
+class GlobeViewModel {
+    var isSnowVisible: Bool = false
+    var isSnowmanVisible: Bool = false
+    var isTreeVisible: Bool = false
+}
