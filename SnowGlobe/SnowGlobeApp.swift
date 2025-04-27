6b7f5956e888a694f72688adeb2bec0ee23d251f
Saverio Negro <saverionegro@gmail.com>
2025-04-27 14:21:11 +0200

    Initial Commit

---
+++ SnowGlobe/SnowGlobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowGlobeApp.swift
+//  SnowGlobe
+//
+//  Created by Saverio Negro on 27/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SnowGlobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
